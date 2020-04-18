7f74ba67bbf59264663052abae198cf1d1bb5be7
Marcus Lau <lau.marcus76@gmail.com>
2020-04-18 20:15:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word(str)
-  rev_str = str.reverse.split(" ")
+  # rev_str = str.reverse.split(" ")
   
-  
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
   
 end
